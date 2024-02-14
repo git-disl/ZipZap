c8a65b5445f76db1539454d0c4df9706695c2555
SihaoHu <husihao26@gmail.com>
2024-02-14 13:42:56 -0500

    Initialization

--- Model/ZipZap_BERT/run_zipzap.sh
+++
@@ -1,6 +0,0 @@
-python gen_seq.py # construct transaction sequence
-python gen_pretrain_data.py # generate pre-training data
-python gen_finetune_data.py # generate fine-tuning data
-
-python run_pretrain.py # pre-training
-python run_finetune.py --init_checkpoint=ckpt_dir/model_64000 # fine-tuning and evaluation
